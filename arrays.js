cc452b01e897b6f2a0382e6503ab81e12cd51a52
Shavonne Holton <shavonne.holton@gmail.com>
2021-06-08 17:18:17 -0700

    completed challenge 4

--- arrays.js
+++ arrays.js
@@ -1,7 +1,5 @@
 // For loop challenge
-
 let foods = ['apple', 'orange', 'rice', 'bread', 'tofu']
-
 for (let i = foods.length - 1; i > -1; i--){
     if (i % 2 == 1){
         let word = foods[i]
@@ -9,30 +7,21 @@ for (let i = foods.length - 1; i > -1; i--){
         console.log(`Foods[${i}]:` + titleCasedWord)
     }
 }
-
 function titleCase(word){
     let first_letter = word[0].toUpperCase()
     let rest = word.slice(1) 
     return first_letter + rest
 }
-
-
 // For..of loop challenge
-
 foods = ['apple', 'orange', 'rice', 'bread', 'tofu']
-
 let allergies = ['gluten', 'soy']
-
 for (let food of foods){
     // assigns a string based on the function returning true / false
     let longWordResult = foodLengthCheck(food) ? "" : "not "
-
     // formatting the output
     let response = `${food} is ${longWordResult}a long word, and ${foodAllergyCheck(food).toLocaleLowerCase()} it with your allergies.`
-
     console.log(response)
 }
-
 function foodLengthCheck(food){
     return food.length > 4
 }
@@ -44,6 +33,7 @@ function foodAllergyCheck(food){
         tofu: "soy"
     }
 
+    allergyMap = { bread: "gluten", tofu: "soy" }
     // Checks to see if food is in the allergyMap
     let allergyLookup = allergyMap[food]
 
@@ -53,12 +43,15 @@ function foodAllergyCheck(food){
     } else { // Else
         return "You can eat"
     }
+    if (allergies.includes(allergyLookup)) return "You can't eat"
+    else return "You can eat" 
 }
 
+
+// Challenge 3
 let cards = [2, 8, "K", 9, 10, 3, 4, "Q", 7, "J", 5, 6, "A"]
 let faceCards = []
 let numberedCards = []
-
 while(cards.length > 0) {
     let card = cards.pop()
     if (isFaceCard(card)){
@@ -67,11 +60,9 @@ while(cards.length > 0) {
         numberedCards.push(card)
     }
 }
-
 console.log(cards)
 console.log(faceCards)
 console.log(numberedCards)
-
 function isFaceCard(card){
     let result;
     switch(card){
@@ -86,4 +77,32 @@ function isFaceCard(card){
             break
     }
     return result
+}
+
+// Challenge 4
+let animals = [
+    "bat", "tiger", "yak",
+    "monkey", "snake", "lizard",
+    "panda", "monkey", "hyena",
+]
+
+let zooTrip = {
+    seen: [],
+    animalsSeen: 0
+}
+
+animals.forEach((animal) => {
+    console.log(`${animal} was seen today at the zoo!`)
+
+    if (!zooTrip.seen.includes(animal)){
+        zooTrip.animalsSeen++
+    } 
+    seenAtTheZoo(animal)
+})
+
+console.log(zooTrip)
+
+
+function seenAtTheZoo(animal){
+    zooTrip.seen.push(animal)
 }
